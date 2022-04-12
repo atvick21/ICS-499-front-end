03250e5d234395f92cc297593e206bec5a9d7176
dnguyen000 <don.nguyen.000@gmail.com>
2022-04-11 20:28:20 -0500

    adding work in progress; have click handler for element and initial setup for compound component

--- src/app/compound/compound.component.ts
+++ src/app/compound/compound.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Element } from '../model/element.model';
 
 @Component({
   selector: 'app-compound',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./compound.component.scss']
 })
 export class CompoundComponent implements OnInit {
+  elementsInCompound: Element[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  addElement(element: Element): void {
+    this.elementsInCompound.push(element);
+  }
+
+  static addElement(element) {
+    this.addElement(element)
+  }
 }
--- src/app/element/element.component.ts
+++ src/app/element/element.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import { Element } from '../model/element.model';
+import { CompoundComponent } from "../compound/compound.component";
 
 @Component({
   selector: 'app-element',
@@ -23,6 +24,14 @@ export class ElementComponent implements OnInit {
   // }
 
   ngOnInit(): void {
+  }
+
+  /*TODO:
+    - Will need to pass information from element in periodic table to element in compound
+    - Should this be handled from the parent (periodic table component) to pass information to the compound component?
+  */
+  onClick(event, className, element): void {
+    console.log("Symbol: \n" + element.symbol)
   }
 
 }
