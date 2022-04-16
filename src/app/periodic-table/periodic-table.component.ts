b9b87e3d6ed9a48b09b2ec4f426cba347976d2cb
calebTree <ande4905@gmail.com>
2022-04-16 01:30:56 -0500

    style and layout overhaul

--- src/app/periodic-table/periodic-table.component.ts
+++ src/app/periodic-table/periodic-table.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Role } from '../enum/role.enum';
-import { User } from '../model/user';
-import { AuthenticationService } from '../service/authentication.service';
 import { Element } from '../model/element.model';
 import { ElementService } from '../service/element.service';
 
@@ -11,60 +7,23 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
+  selector: 'periodic-table',
   templateUrl: './periodic-table.component.html',
   styleUrls: ['./periodic-table.component.scss']
 })
 export class PeriodicTableComponent implements OnInit, OnDestroy {
   private subs = new SubSink();
   showFiller = false;
-  public user: User;
-  public isLoggedIn: boolean;
   elements: Element[] = [];
-  pageTitle:string ='Sandbox'
+  pageTitle: string = 'Sandbox'
   
-  constructor(private elementService: ElementService, private router: Router, private authService: AuthenticationService,
-    private _snackBar: MatSnackBar) { }
+  constructor(private elementService: ElementService, private _snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
-    if (this.authService.isUserLoggedIn()) {
-      this.user = this.authService.getUserFromLocalCache();
-      this.isLoggedIn = true;
-    } else {
-      this.isLoggedIn = false;
-    }
     this.getElements(true);
   }
 
-  public onClickLogin(): void {
-    this.router.navigate(['/login']);
-  }
-
-  public onClickLogout(): void {
-    this.authService.logOut();
-    this.isLoggedIn = false;
-    this.user = null;
-  }
-
-  public onClickUserManagement(): void {
-    this.router.navigate(['/user/management']);
-  }
-
-  public get isAdmin(): boolean {
-    if(this.isLoggedIn)
-      return this.getUserRole() === Role.ADMIN || this.getUserRole() === Role.SUPER_ADMIN;
-    else return false;
-  }
-
-  private getUserRole(): string {
-    return this.authService.getUserFromLocalCache().role;
-  }
-
   async getElements(showNotification: boolean) {
-    // const data = this.ElementService
-    //   .getElements()
-    //   .subscribe((data: Element[]) => this.elements = data.sort((a, b) => (Number(a.atomicNumber) > Number(b.atomicNumber) ? 1 : -1)))
-    // console.log("data returned from API call: \n" + data);
-    
     this.subs.add(
       this.elementService.getElements().subscribe({
         next: (response: Element[]) => {
