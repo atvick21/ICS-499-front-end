18d26e9d6d669582760b8cce0a0bf9abe25c308e
calebTree <ande4905@gmail.com>
2022-04-30 11:08:56 -0500

    return quizes by userId

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -4,6 +4,7 @@ import { Quiz } from '../model/quiz';
 import { QuizService } from '../service/quiz.service';
 import {HttpErrorResponse} from "@angular/common/http";
 import {MatSnackBar} from "@angular/material/snack-bar";
+import { AuthenticationService } from '../service/authentication.service';
 
 
 @Component({
@@ -11,40 +12,41 @@ import {MatSnackBar} from "@angular/material/snack-bar";
   styleUrls: ['./quiz.component.scss']
 })
 export class QuizComponent implements OnInit {
-  quiz : Quiz[] =[]
+  quiz : Quiz[] = [];
   question: string;
-  currentQuiz =0;
+  currentQuiz = 0;
   answers: any;
   answerSelected: boolean;
-  correctAnswers  =0;
-  incorrectAnswers =0;
+  correctAnswers = 0;
+  incorrectAnswers = 0;
   score = false;
   random : number;
-  constructor(private quizService: QuizService, private _snackBar: MatSnackBar) { }
+  constructor(private quizService: QuizService, private _snackBar: MatSnackBar, private authenticationService: AuthenticationService) { }
 
-    ngOnInit(): void {
-      this.quizService.getQuiz().subscribe(data => {
-        this.quiz = data;
-      });
-      this.random = Math.floor(Math.random()*this.quiz.length);
-      }
-  onAnswer(option: boolean){
+  ngOnInit(): void {
+    this.quizService.getQuizByUserId(this.authenticationService.getUserFromLocalCache().userId).subscribe(data => {
+      this.quiz = data;
+    });
+    this.random = Math.floor(Math.random()*this.quiz.length);
+  }
+
+  onAnswer(option: boolean) {
     this.answerSelected =true;
-    setTimeout(()=>{
+
+    setTimeout(() => {
       this.currentQuiz++;
       this.random = Math.floor(Math.random() *this.quiz.length);
       this.answerSelected = false;
 
-    },6000);
-    if(option){
+    }, 6000);
+
+    if(option) {
       this.correctAnswers++;
-    }
-    else{
+    } else {
       this.incorrectAnswers++;
     }
-
-
   }
+
   displayScore(){
     this.score= true;
   }
