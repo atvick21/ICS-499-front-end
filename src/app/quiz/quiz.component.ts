45c39d29a31ec37f9d9db9fd72b63bba8ff49a75
calebTree <ande4905@gmail.com>
2022-05-01 21:41:54 -0500

    quiz refactor

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -5,7 +5,7 @@ import { QuizService } from '../service/quiz.service';
 import {HttpErrorResponse} from "@angular/common/http";
 import {MatSnackBar} from "@angular/material/snack-bar";
 import { AuthenticationService } from '../service/authentication.service';
-
+import { NgForm } from '@angular/forms';
 
 @Component({
   templateUrl: './quiz.component.html',
@@ -15,13 +15,13 @@ export class QuizComponent implements OnInit {
 
   quizzes : Quiz[] = [];
   question: string;
-  currentQuiz = 0;
+  currentQuiz: number = 0;
   answer: any;
   answerSelected: boolean;
-  correctAnswers = 0;
-  incorrectAnswers = 0;
-  score = false;
-  random : number;
+  correctAnswers: number = 0;
+  incorrectAnswers: number = 0;
+  score: boolean = false;
+  random: number;
   
   constructor(private quizService: QuizService, private _snackBar: MatSnackBar, private authenticationService: AuthenticationService) { }
 
@@ -32,26 +32,32 @@ export class QuizComponent implements OnInit {
     this.random = Math.floor(Math.random()*this.quizzes.length);
   }
 
-  onAnswer(answer: string): boolean {
+  onSubmit(form: NgForm) {
     this.answerSelected =true;
 
     setTimeout(() => {
       this.currentQuiz++;
       this.random = Math.floor(Math.random() *this.quizzes.length);
       this.answerSelected = false;
+      // unselect radio buttons
+      for(let i = 0; i < document.getElementsByName("answers").length; i++) {
+        const ele = document.getElementsByName("answers")[i] as HTMLInputElement;
+        ele.checked = false;
+      }
     }, 6000);
 
-    if (answer == this.quizzes[this.random].answer) {
+    if (form.value.answers == this.quizzes[this.random].answer) {
       this.correctAnswers++;
       return true;
     } else {
       this.incorrectAnswers++;
       return false;
     }
+    
   }
 
   displayScore() {
-    this.score = true;
+    this.score = !this.score;
   }
 
 }
